09e530f6f07ce9c062324ac4338e87b171f78ffa
Trevor Sears <trevorsears.main@gmail.com>
2019-10-11 14:41:23 -0400

    Added documentation and 'results' message.
    Also fixed a bug where using the '-c' flag would change Python's working directory, causing later code to search the wrong path.

--- up2date/main.py
+++ up2date/main.py
@@ -19,15 +19,18 @@ def is_git_repo(repo_path: str) -> bool:
 
 def get_timestamp_from_git_repo(repo_path: str) -> int:
 	
+	previous_working_directory = os.getcwd()
+	
 	if is_git_repo(repo_path):
 		os.chdir(repo_path)
 		result = subprocess.run(["git", "log", "-1", "--format=%ct"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+		os.chdir(previous_working_directory)
 		return int(result)
 	
 	else:
 		raise ValueError("Provided path was not a git repository directory.")
 
-def find_and_replace_in_file(file_path: str, content: str, replacement: str) -> None:
+def find_and_replace_in_file(file_path: str, content: str, replacement: str) -> int:
 	
 	if path.exists(file_path) and path.isfile(file_path):
 
@@ -36,16 +39,26 @@ def find_and_replace_in_file(file_path: str, content: str, replacement: str) -> 
 		with open(file_path) as f:
 
 			lines = f.readlines()
-
-		lines = [line.replace(content, replacement) for line in lines]
+			
+		modified_lines = []
+		modifications = 0
+		
+		for line in lines:
+			while content in line:
+				line = line.replace(content, replacement, 1)
+				modifications += 1
+			modified_lines.append(line)
 
 		with open(file_path, "w") as f:
 
-			f.writelines(lines)
+			f.writelines(modified_lines)
+			
+		return modifications
 
 	else:
 
 		print("Could not find file in order to replace text: '" + file_path + "'")
+		return 0
 	
 def run_cli() -> None:
 	
@@ -70,22 +83,32 @@ def run_cli() -> None:
 	orig_text, new_text = arguments[-2:]
 	files = arguments[1:-2]
 	
-	print("Git repo: " + git_repo)
-	print("Last modified: " + str(get_timestamp_from_git_repo(git_repo)))
-	print("Orig text: " + orig_text)
-	print("New text: " + new_text)
-	print("Files: " + str(files))
-	
 	if not is_git_repo(git_repo):
+		print(path.abspath(git_repo))
 		invalid_git_repo(git_repo)
 		exit(1)
 		
 	if should_fail_silently:
 		files = [file for file in files if path.exists(file) and path.isfile(file)]
 		
-	run(git_repo, files, orig_text, new_text)
+	modifications = run(git_repo, files, orig_text, new_text)
+	
+	num_of_files = len(files)
+	print("Successfully performed " + str(modifications) + " modification" + ("s" if modifications != 1 else "") +
+			" over " + str(num_of_files) + " file" + ("s" if num_of_files != 1 else "") + ".")
 
-def run(repo_path: str, files: List[str], orig_text: str, new_text: str) -> None:
+def run(repo_path: str, files: List[str], orig_text: str, new_text: str) -> int:
+	
+	"""
+	Finds and replaces all occurrences of the 'orig_text' string with the 'new_text' string in each of the file paths
+	specified if the file has been updated since the last commit to the specified repository.
+	
+	:param repo_path: A path to a valid Git repository.
+	:param files: A list of files to conditionally find-and-replace in.
+	:param orig_text: The text to replace.
+	:param new_text: The text with which to replace.
+	:return: The total number of find-and-replace operations that occurred.
+	"""
 	
 	if not is_git_repo(repo_path):
 		
@@ -97,10 +120,14 @@ def run(repo_path: str, files: List[str], orig_text: str, new_text: str) -> None
 		
 		raise ValueError("Some provided input file was invalid/did not exist.")
 	
+	modifications = 0
+	
 	for file in files:
 		
 		last_modification_timestamp = path.getmtime(file)
 		
 		if last_modification_timestamp > last_commit_timestamp:
 			
-			find_and_replace_in_file(file, orig_text, new_text)
+			modifications += find_and_replace_in_file(file, orig_text, new_text)
+			
+	return modifications
